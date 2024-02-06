c67196f65224625b9d41b2cd53358bb7dd351372
yoshiki-na <oh-nakamura@KDDI-SDEV-N-017.local>
2024-02-06 12:00:37 +0900

    setup knex and make migration file

---
+++ db/migrations/20240206025131_create_books_table.js
@@ -0,0 +1,29 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.up = function (knex) {
+  /*
+   * id increments
+   * name varchar (100)
+   * genre varchar (100)
+   * recommender_name varchar (100)
+   * recommender_memo varchar (255)
+   */
+  return knex.schema.createTable("books", function (table) {
+    table.increments("id").primary();
+    table.string("name", 100);
+    table.string("genre", 100);
+    table.string("recommender_name", 255);
+    table.string("recommender_memo", 255);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("books");
+};
